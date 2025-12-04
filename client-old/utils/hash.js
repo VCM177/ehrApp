91e20800b0bafceb97569e9c4741915a2852bd84
ChiMinh <vochiminh177@gmail.com>
2025-12-04 10:03:00 +0700

    Delete client-old

--- client-old/utils/hash.js
+++
@@ -1,6 +0,0 @@
-import CryptoJS from 'crypto-js';
-
-// Hash a string using SHA256 and convert to hex format
-export const hashSHA256 = (text) => {
-    return '0x' + CryptoJS.SHA256(text).toString();
-};
